bdb41f8f072ea3905af870d371934b0dd6540b1c
Woo63 <55555r@list.ru>
2019-08-15 19:49:32 +0400

    first version

---
+++ index.js
@@ -0,0 +1,42 @@
+form.onsubmit = function (e) {
+    e.preventDefault();
+    var body = {
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+    };
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'https://us-central1-mercdev-academy.cloudfunctions.net/login', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function () {
+        var res = JSON.parse(xhr.responseText);
+        if (res.error == "Invalid email or password") {
+            document.getElementById("email").style.border = 'solid 1px #ed4159';
+            document.getElementById("email").style.color = '#ed4159';
+            document.getElementById("warning").style.display = 'block';
+        }
+        else {
+            form.remove();
+            var photo = document.createElement('img');
+            photo.id = "avatar";
+            photo.src = res.photoUrl;
+            var name = document.createElement('p');
+            name.innerText = res.name;
+            name.id = "userName";
+            var button_out = document.createElement('button');
+            button_out.className = "form_button";
+            button_out.onclick = z;
+            button_out.innerText = "Logout";
+            var div = document.createElement('div');
+            div.id = "form";
+            div.append(photo);
+            div.append(name);
+            div.append(button_out);
+            document.body.appendChild(div);
+        }
+    };
+    xhr.send(JSON.stringify(body));
+};
+
+function z() {
+    window.location.reload();
+}
